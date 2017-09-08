b27718007d66804f1f75bcac8594c5e5ad11d43f
Johannes Sjölund <j.sjolund@gmail.com>
2017-09-08 15:17:21 +0200

    Add closest contacts test

--- src/routingtable_test.go
+++ src/routingtable_test.go
@@ -22,14 +22,53 @@ func TestRoutingTable(t *testing.T) {
 	}
 }
 
+func TestRoutingTableFindClosestContacts(t *testing.T) {
+	const numContacts = 0x42
+	const portStart = 8000
+	const maxResults = 20
+	const toFind = 0x232323
+
+	idToFind := NewKademliaID(fmt.Sprintf("%040x", toFind));
+	var allcontacts [numContacts]Contact
+
+	// Create a routing table with some contacts, also store all contacts for comparison
+	me := NewContact(NewKademliaID(fmt.Sprintf("%040x", 0)), fmt.Sprintf("localhost:%04d", portStart))
+	allcontacts[0] = me
+	rt := NewRoutingTable(me)
+	for i := 1; i < numContacts; i++ {
+		contact := NewContact(NewKademliaID(fmt.Sprintf("%040x", i)), fmt.Sprintf("localhost:%04d", portStart+i))
+		allcontacts[i] = contact
+		rt.AddContact(contact)
+	}
+	// Find the closest contacts to 'toFind'. Contacts are be sorted (test?) according to min distance
+	contacts := rt.FindClosestContacts(idToFind, maxResults)
+	minDistance := contacts[0].distance
+	// Check all contacts if there are any closer
+	for i := range allcontacts {
+		contact := allcontacts[i]
+		// Use the algorithm from the paper
+		distance := KademliaID{}
+		for i := 0; i < IDLength; i++ {
+			distance[i] = idToFind[i] ^ contact.ID[i]
+		}
+		for i := 0; i < IDLength; i++ {
+			if idToFind[i] < minDistance[i] {
+				t.Fail()
+			}
+		}
+		// Try the API functions for it
+		if idToFind.CalcDistance(contact.ID).Less(minDistance) {
+			t.Fail()
+		}
+	}
+}
+
 func TestFileHash(t *testing.T) {
 	decoded := "9ae65414b4803a999452f0c320eb41bec1e14bc1"
-	fileHash,err := NewKademliaIDFromFile("test.bin")
+	fileHash, err := NewKademliaIDFromFile("test.bin")
 	if err != nil {
 		t.Fail()
 	} else if hex.EncodeToString(fileHash[0:IDLength]) != decoded {
 		t.Fail()
-	} else {
-		fmt.Println(fileHash.String())
 	}
 }
