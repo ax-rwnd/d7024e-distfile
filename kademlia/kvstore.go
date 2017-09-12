820a8ae7b4968e6a2a630d694537e3f31cf3c844
Johannes Sjölund <j.sjolund@gmail.com>
2017-09-13 00:16:19 +0200

    KVStore is no longer global, must be unique in unit tests.

--- kademlia/kvstore.go
+++ kademlia/kvstore.go
@@ -12,49 +12,44 @@ var NotFoundError = errors.New("Value was not found in map.")
 
 // Globals
 var EvictionTime, _ = time.ParseDuration("1h")
-var KVStore map[KademliaID]kvData
 
+type KVStore map[KademliaID]kvData
 type kvData struct {
     data      []byte
     timestamp time.Time
     pinned    bool
 }
 
-// Manage the initialization of the storage
-func KVSInit() (err error) {
-    if KVStore != nil {
-        return errors.New("KVS already initialized!")
-    }
-    KVStore = make(map[KademliaID]kvData)
-    return
+func NewKVStore() KVStore {
+    return make(map[KademliaID]kvData)
 }
 
 // Evict data after some time
-func KVSEvict(now time.Time) (err error) {
-    for key, value := range KVStore {
+func (kvStore KVStore) Evict(now time.Time) (err error) {
+    for key, value := range kvStore {
         if now.Sub(value.timestamp) > EvictionTime && !value.pinned {
-            delete(KVStore, key) //TODO: check for runtimes
+            delete(kvStore, key) //TODO: check for runtimes
         }
     }
     return
 }
 
 // Don't silently update duplicate data (in case of collision)
-func KVSInsert(hash KademliaID, pinned bool, data []byte) (outData kvData, err error) {
-    if KVStore == nil {
+func (kvStore KVStore) Insert(hash KademliaID, pinned bool, data []byte) (outData kvData, err error) {
+    if kvStore == nil {
         err = NotInitializedError
-    } else if _, ok := KVStore[hash]; ok {
+    } else if _, ok := kvStore[hash]; ok {
         err = DuplicateError
     } else {
         outData = kvData{data: data, timestamp: time.Now(), pinned: pinned}
-        KVStore[hash] = outData
+        kvStore[hash] = outData
     }
     return
 }
 
 // Lookup data from table
-func KVSLookup(hash KademliaID) (output []byte, err error) {
-    if val, ok := KVStore[hash]; ok {
+func (kvStore KVStore) Lookup(hash KademliaID) (output []byte, err error) {
+    if val, ok := kvStore[hash]; ok {
         output = val.data
     } else {
         err = NotFoundError
@@ -62,8 +57,8 @@ func KVSLookup(hash KademliaID) (output []byte, err error) {
     return
 }
 
-func KVSClear() {
-    for k := range KVStore {
-        delete(KVStore, k)
+func (kvStore KVStore) Clear() {
+    for k := range kvStore {
+        delete(kvStore, k)
     }
 }
--- kademlia/kvstore_test.go
+++ kademlia/kvstore_test.go
@@ -5,29 +5,17 @@ import (
     "fmt"
 )
 
-func TestInit(t *testing.T) {
-    if err := KVSInit(); err != nil {
-        fmt.Println("Initialization of KVStore failed.")
-        t.Fail()
-    }
-
-    if err := KVSInit(); err == nil {
-        fmt.Println("KVStore was silently reinitialized!")
-        t.Fail()
-    }
-}
-
 func TestKVSInsertLookup(t *testing.T) {
-    KVSClear()
+    kvStore := NewKVStore()
     data := []byte("Test data")
     id := NewKademliaIDFromBytes(data)
     pinned := false
-    _, err := KVSInsert(*id, pinned, data)
+    _, err := kvStore.Insert(*id, pinned, data)
     if err != nil {
         fmt.Println(err)
         t.Fail()
     }
-    storedData, err := KVSLookup(*id)
+    storedData, err := kvStore.Lookup(*id)
     if err != nil {
         fmt.Println(err)
         t.Fail()
@@ -40,11 +28,11 @@ func TestKVSInsertLookup(t *testing.T) {
 }
 
 func TestNotFoundError(t *testing.T) {
-    KVSClear()
+    kvStore := NewKVStore()
     data := []byte("Test data")
     id := NewKademliaIDFromBytes(data)
     // Lookup without inserting first
-    _, err := KVSLookup(*id)
+    _, err := kvStore.Lookup(*id)
     if err != NotFoundError {
         fmt.Println("Wrong error")
         fmt.Println(err)
@@ -53,20 +41,19 @@ func TestNotFoundError(t *testing.T) {
 }
 
 func TestDuplicateError(t *testing.T) {
-    KVSClear()
+    kvStore := NewKVStore()
     data := []byte("Test data")
     id := NewKademliaIDFromBytes(data)
     pinned := false
-    _, err := KVSInsert(*id, pinned, data)
+    _, err := kvStore.Insert(*id, pinned, data)
     if err != nil {
         fmt.Println(err)
         t.Fail()
     }
-    _, err = KVSInsert(*id, pinned, data)
+    _, err = kvStore.Insert(*id, pinned, data)
     if err != DuplicateError {
         fmt.Println("Wrong error")
         fmt.Println(err)
         t.Fail()
     }
-    delete(KVStore, *id)
 }
