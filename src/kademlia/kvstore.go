31dce39d4b92121b9b95c8173ce5ac507b39f6e2
jsjolund <j.sjolund@gmail.com>
2017-10-07 12:46:12 +0200

    Key value store timer evicts non pinned stuff

--- src/kademlia/kvstore.go
+++ src/kademlia/kvstore.go
@@ -4,6 +4,7 @@ import (
     "time"
     "errors"
     "sync"
+    "fmt"
 )
 
 // Error states
@@ -12,53 +13,88 @@ var DuplicateError = errors.New("value is already in map")
 var NotFoundError = errors.New("value was not found in map")
 
 // Globals
-var EvictionTime, _ = time.ParseDuration("1h")
-
-type KVStore struct {
-    mapping map[KademliaID]kvData
-    mutex   *sync.Mutex
-}
+var EvictionTime = 24 * time.Hour
 
 type kvData struct {
-    data      []byte
-    timestamp time.Time
-    pinned    bool
+    id         KademliaID
+    data       []byte
+    timeToLive time.Time
+    pinned     bool
+}
+
+type kvTimeToLiveQueue []kvData
+
+type KVStore struct {
+    timer           *time.Timer
+    timeToLiveQueue kvTimeToLiveQueue
+    mapping         map[KademliaID]kvData
+    mutex           *sync.Mutex
+}
+
+func (s kvTimeToLiveQueue) Len() int {
+    return len(s)
+}
+
+func (s kvTimeToLiveQueue) Swap(i, j int) {
+    s[i], s[j] = s[j], s[i]
+}
+
+func (s kvTimeToLiveQueue) Less(i, j int) bool {
+    return s[i].timeToLive.Before(s[j].timeToLive)
 }
 
 func NewKVStore() *KVStore {
     kvStore := new(KVStore)
     kvStore.mutex = &sync.Mutex{}
     kvStore.mapping = make(map[KademliaID]kvData)
+    kvStore.timeToLiveQueue = []kvData{}
+    kvStore.timer = time.NewTimer(0)
+    <-kvStore.timer.C
+    go kvStore.eviction()
     return kvStore
 }
 
-// Evict data after some time
-func (kvStore KVStore) Evict(now time.Time) (err error) {
+func (kvStore *KVStore) eviction() {
+    for {
+        <-kvStore.timer.C
+        kvStore.mutex.Lock()
+        fmt.Println("Checking")
+        if len(kvStore.timeToLiveQueue) > 0 {
+            toEvict := kvStore.timeToLiveQueue[0]
+            kvStore.timeToLiveQueue = kvStore.timeToLiveQueue[1:]
+            delete(kvStore.mapping, toEvict.id)
+            fmt.Printf("Evicted %v\n", toEvict.id.String())
+        }
+        if len(kvStore.timeToLiveQueue) > 0 {
+            nextEvictionDuration := kvStore.timeToLiveQueue[0].timeToLive.Sub(time.Now())
+            kvStore.timer.Reset(nextEvictionDuration)
+        }
+        kvStore.mutex.Unlock()
+    }
+}
+
+// Don't silently update duplicate data (in case of collision)
+func (kvStore *KVStore) Insert(hash KademliaID, pinned bool, data []byte) (outData kvData, err error) {
     kvStore.mutex.Lock()
-    for key, value := range kvStore.mapping {
-        if now.Sub(value.timestamp) > EvictionTime && !value.pinned {
-            delete(kvStore.mapping, key) //TODO: check for runtimes
+    if kvStore.mapping == nil {
+        err = NotInitializedError
+    } else {
+        outData = kvData{id: hash, data: data, timeToLive: time.Now().Add(EvictionTime), pinned: pinned}
+        kvStore.mapping[hash] = outData
+        if !pinned {
+            kvStore.timeToLiveQueue = append(kvStore.timeToLiveQueue, outData)
+            if len(kvStore.timeToLiveQueue) == 1 {
+                nextEvictionDuration := kvStore.timeToLiveQueue[0].timeToLive.Sub(time.Now())
+                kvStore.timer.Reset(nextEvictionDuration)
+            }
         }
     }
     kvStore.mutex.Unlock()
     return
 }
 
-// Don't silently update duplicate data (in case of collision)
-func (kvStore KVStore) Insert(hash KademliaID, pinned bool, data []byte) (outData kvData, err error) {
-    kvStore.mutex.Lock()
-    if kvStore.mapping == nil {
-        err = NotInitializedError
-    } else {
-        outData = kvData{data: data, timestamp: time.Now(), pinned: pinned}
-        kvStore.mapping[hash] = outData
-    }
-    kvStore.mutex.Unlock()
-    return
-}
-
 // Lookup data from table
-func (kvStore KVStore) Lookup(hash KademliaID) (output []byte, err error) {
+func (kvStore *KVStore) Lookup(hash KademliaID) (output []byte, err error) {
     kvStore.mutex.Lock()
     if val, ok := kvStore.mapping[hash]; ok {
         output = val.data
--- src/kademlia/kvstore_test.go
+++ src/kademlia/kvstore_test.go
@@ -3,6 +3,8 @@ package kademlia
 import (
     "testing"
     "fmt"
+    "time"
+    "log"
 )
 
 func TestKVSInsertLookup(t *testing.T) {
@@ -27,7 +29,7 @@ func TestKVSInsertLookup(t *testing.T) {
     }
 }
 
-func TestNotFoundError(t *testing.T) {
+func TestKVSNotFoundError(t *testing.T) {
     kvStore := NewKVStore()
     data := []byte("Test data")
     id := NewKademliaIDFromBytes(data)
@@ -37,5 +39,58 @@ func TestNotFoundError(t *testing.T) {
         fmt.Println("Wrong error")
         fmt.Println(err)
         t.Fail()
+    }
+}
+
+func TestKVSTimeToLiveNoPin(t *testing.T) {
+    // Store some non pinned data
+    kvStore := NewKVStore()
+    data1 := []byte("Test data1")
+    data2 := []byte("Test data2")
+    id1 := NewKademliaIDFromBytes(data1)
+    id2 := NewKademliaIDFromBytes(data2)
+    pinned := false
+
+    // Set a short eviction time for testing
+    EvictionTime = 3 * time.Second
+
+    // Add some data
+    fmt.Printf("Inserted %v\n", id1.String())
+    kvStore.Insert(*id1, pinned, data1)
+    // Wait two seconds, then check that data is still there
+    timer := time.NewTimer(2 * time.Second)
+    <-timer.C
+    if _, err := kvStore.Lookup(*id1); err != nil {
+        t.Fail()
+        log.Println("ID1 was removed too early")
+    }
+
+    // Insert some more data before previous is evicted
+    fmt.Printf("Inserted %v\n", id2.String())
+    kvStore.Insert(*id2, pinned, data2)
+
+    // Wait until ID1 should have been evicted
+    // Check that ID1 was removed before ID2
+    timer = time.NewTimer(2 * time.Second)
+    <-timer.C
+    if _, err := kvStore.Lookup(*id1); err == nil {
+        t.Fail()
+        log.Println("ID1 was not removed")
+    }
+    if _, err := kvStore.Lookup(*id2); err != nil {
+        t.Fail()
+        log.Println("ID2 was removed too early")
+    }
+
+    // Wait until both eviction times have passed, then check data is gone
+    timer = time.NewTimer(2 * time.Second)
+    <-timer.C
+    if _, err := kvStore.Lookup(*id1); err == nil {
+        t.Fail()
+        log.Println("ID1 was not removed")
+    }
+    if _, err := kvStore.Lookup(*id2); err == nil {
+        t.Fail()
+        log.Println("ID2 was not removed")
     }
 }
