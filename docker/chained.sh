e7a37d61ecfccb78e8b8afb79eb84e9f5f626432
Axel <axel-sundbom@hotmail.com>
2017-10-09 14:07:34 +0200

    Updates two system tests

--- docker/chained.sh
+++ docker/chained.sh
@@ -5,6 +5,8 @@ make build
 
 for i in `seq 2 $1`;
 do
-    docker run -d --rm --workdir /kademlia --net kademnet -e "KADIP=172.18.0.$i" --ip 172.18.0.$i d7024e_deploy sh -c "./kademliad" 
+    let last=$i-1
+    echo "Starting $i which bootstraps $last"
+    docker run -d --rm --workdir /kademlia --net kademnet -e "KADIP=172.18.0.$i" -e "BOOTIP=172.18.0.$last" --ip 172.18.0.$i d7024e_deploy sh -c "./kademliad" 
     sleep .5s
 done
